fbb019b488b09cc442f4d743e24bcd2d9ae09b57
Colin James Wood <christhefennec@gmail.com>
2025-08-18 11:51:09 +0100

    modded scaling bullets are no longer able to be gotten in survival by default without datapacking and external mods

--- src/main/java/xyz/kaleidiodev/kaleidiosguns/event/ModTradesEvent.java
+++ src/main/java/xyz/kaleidiodev/kaleidiosguns/event/ModTradesEvent.java
@@ -42,10 +42,10 @@ public class ModTradesEvent {
         //    trades.get(5).add((trader, rand) -> new MerchantOffer(new ItemStack(Items.EMERALD, 50), new ItemStack(ModItems.heroShotgun, 1), 1, 80, 0.3F));
         //}
 
-        if (event.getType() == VillagerProfession.CLERIC) {
-            Int2ObjectMap<List<VillagerTrades.ITrade>> trades = event.getTrades();
+        //if (event.getType() == VillagerProfession.CLERIC) {
+        //    Int2ObjectMap<List<VillagerTrades.ITrade>> trades = event.getTrades();
 
-            trades.get(5).add((trader, rand) -> new MerchantOffer(new ItemStack(Items.EMERALD, 45), new ItemStack(ModItems.xpBullet, 1), 10, 32, 0.3F));
-        }
+        //    trades.get(5).add((trader, rand) -> new MerchantOffer(new ItemStack(Items.EMERALD, 45), new ItemStack(ModItems.xpBullet, 1), 10, 32, 0.3F));
+        //}
     }
 }
